8ef98f1a1b69db4b0755b413d7b38ad04e5e8577
cmh395 <65511672+cmh395@users.noreply.github.com>
2020-05-31 18:26:51 -0400

    Add files via upload

---
+++ bEuropaAtmosphericModel.m
@@ -0,0 +1,23 @@
+function drag = bEuropaAtmosphericModel(height,velocity)
+
+g = 1.315; %m/s^2
+gamma = 1.4; %O2 atmosphere, diatomic value
+Ru = 8314.5; 
+MW = 32; %g/cm^3
+
+numDens = 10e8; %number density of oxygen on surface, particles/cm^3
+mols = 6.02e23; %avogadro's number
+
+surfaceDensity = numDens*MW/mols*1000; %surface density in kg/m^3
+R = Ru/MW; 
+T0 = 100; %K
+
+Cd = 1.5; 
+radius = 2.65/2; %m
+A = pi*radius^2; 
+
+density = surfaceDensity*(1-(gamma-1)*g*height/(gamma*R*T0))^(1/(gamma-1)); 
+
+drag = Cd*density*velocity^2*A/2*1000^2; 
+%fprintf('Drag = %f N\n', drag)
+end
